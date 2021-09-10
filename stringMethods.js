a45db8824eee51293c9b2afe80752fc918e79fb9
Mihir <lucasgreat69@gmail.com>
2021-09-10 11:40:30 -0700

    Create stringMethods.js

---
+++ stringMethods.js
@@ -0,0 +1,46 @@
+/////Extracting string parts
+// let str = "Apple, Banana, Kiwi";
+// const newStr=str.slice(-12, -10);
+// console.log(newStr);
+
+// //////replace
+// let str = "Apple, Banana, Kiwi";
+// let str2 = "berry";
+// console.log(str.replace("Apple", str2));
+
+/////////////////string methods
+
+//trim
+// let str = "   Apple, Banana, Kiwi    ";
+// console.log(str.trim());
+
+
+///padStart and padEnd
+// let str = "5";
+// console.log(str.padStart(4,0));
+
+///padEnd
+// let str = "5";
+// console.log(str.padEnd(4,0));
+
+
+/////////extracting string character
+// let str = "Apple, Banana, Kiwi";
+// console.log(str[0])
+
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.charAt(1))
+
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.charCodeAt(1))
+
+
+
+///the substring method
+
+//////////////converting string to array
+// let text = "HELLO WORLD";
+// console.log(text.split(""))
+
+// let text = "HELLO WORLD";
+// console.log(text.split(" ,"))
